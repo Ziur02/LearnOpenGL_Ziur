872e4956fe2df6d3b62b2dfe59bee40f9e392ce2
之源 李 <1144620424@qq.com>
2022-08-02 04:21:00 +0800

    完成了hello window章节

--- LearnOpenGL/01_getting_started/01_hello_window.cpp
+++ LearnOpenGL/01_getting_started/01_hello_window.cpp
@@ -5,8 +5,9 @@
 #include <GLFW/glfw3.h>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
 
-//窗口大小设置
+// 窗口大小设置
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
@@ -41,16 +42,30 @@ int main()
 	// 渲染循环(Render Loop)
 	while (!glfwWindowShouldClose(window))
 	{
-		glfwSwapBuffers(window);
+		// 输入
+		processInput(window);
+
+		// 渲染指令
+		glClearColor(0.8f, 0, 0.4f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// 检查并调用事件，交换缓冲
 		glfwPollEvents();
+		glfwSwapBuffers(window);
 	}
 
 	// 当渲染循环结束后，释放之前的资源
 	glfwTerminate();
 	return 0;
 }
+// 当按下ESC键时关闭窗口
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
 
-//当窗口大小发生变化时，执行以下回调函数调整窗口大小
+// 当窗口大小发生变化时，执行以下函数调整窗口大小，是一个回调函数
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
